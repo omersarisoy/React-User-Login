b875bda5d3b00360e4b0af06a1f9b9cc32c9e114
Ömer Sarısoy <omer.sarisoy@asnak.com>
2022-10-05 14:51:19 +0300

    login-1

---
+++ login-page/src/App.js
@@ -0,0 +1,11 @@
+import { LoginWrap } from "./styled.js";
+
+function App() {
+  return (
+    <LoginWrap>
+      <div className="login"></div>
+    </LoginWrap>
+  );
+}
+
+export default App;
---
+++ login-page/src/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LoginWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  div {
+    border: 1px solid red;
+    width: 500px;
+    height: 600px;
+  }
+`;
