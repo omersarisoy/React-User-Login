e42f5479c472897fc521e5ef852f3b4b7047db37
Ömer Sarısoy <omer.sarisoy@asnak.com>
2022-10-10 14:26:23 +0300

    form

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -1,10 +1,91 @@
-import { LoginWrap } from "./styled.js";
+import { useState } from "react";
+import { GlobalStyle, LoginWrap } from "./styled.js";
 
 function App() {
+  const userRegex = /^[a-zA-Z\- ]+$/
+  const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+  const [fullName, setFullName] = useState()
+  const [companyName, setCompanyName] = useState()
+  const [emailName, setEmailName] = useState()
+
+  const validateName = (name) => {
+    return userRegex.test(name)
+  }
+
+  const validateEmail = (name) => {
+    return emailPattern.test(name) 
+  }
+
+
+  const formVal = document.getElementById("form")
+  formVal?.addEventListener("submit", e => {
+    e.preventDefault();
+    validateName();
+    validateEmail()
+
+    })
+  // console.log('form', form)
+
+  function handleSubmit(e) {
+    // console.log('e', e)
+    e.preventDefault();
+    validateName(fullName);
+    validateName(companyName);
+    validateEmail(emailName)
+  }
+
+
   return (
+    <>
+    <GlobalStyle />
     <LoginWrap>
-      <div className="login"></div>
+      <div className="login">
+        <div className="header">
+          <p><strong>WORD</strong>LINE</p>
+          <p>Create Your Test Account</p>
+        </div>
+        <form id="form" action="/action_page.php" onSubmit={e => handleSubmit(e)}>
+          <div>
+            <label for="fname">Full name</label>
+            <input 
+              type="text" 
+              id="fname" 
+              name="fname" 
+              value={fullName}
+              onChange={e => setFullName(e.target.value)}
+              />
+            <span className={validateName(fullName) ? "validMsg" : "errMsg"} >Please Enter Correct Format</span>
+          </div>
+          <div>
+            <label for="lname">Company name</label>
+            <input 
+              type="text" 
+              id="lname"
+              name="lname"
+              value={companyName}
+              onChange={e => setCompanyName(e.target.value)}
+            />
+            <span className={validateName(companyName) ? "validMsg" : "errMsg"} >Please Enter Correct Format</span>
+          </div>
+          <div>
+            <label for="email">Email address</label>
+            <input 
+              type="email" 
+              id="umail" 
+              name="umail"
+              value={emailName}
+              onChange={e => setEmailName(e.target.value)}
+              />
+              <span className={validateEmail(emailName) ? "validMsg" : "errMsg"} >Please Enter Correct Format</span>
+          </div>
+          <div>
+            <input className="btn" type="submit" value="Create an account" />
+          </div>
+        </form>
+      </div>
     </LoginWrap>
+    </>
   );
 }
 
--- login-page/src/styled.js
+++ login-page/src/styled.js
@@ -1,12 +1,65 @@
 import styled from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 export const LoginWrap = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  div {
-    border: 1px solid red;
-    width: 500px;
-    height: 600px;
+  height: 100vh;
+  .login {
+    border: 1px solid gray;
+    width: 400px;
+    border-radius: 8px;
+    box-shadow: 1px 1px 12px 4px rgb(92 88 92 / 51%);
+    padding: 10px 50px 60px 50px;
+  }
+  .header {
+    p:nth-of-type(1) {
+      font-size: 36px;
+      color: #3a893a;
+      margin-bottom: 0;
+    }
+    p:nth-of-type(2) {
+      font-size: 24px;
+      margin-top: 0;
+    }
+  } 
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+
+    label {
+      color: gray;
+      display: block;
+    }
+    input {
+      width: 100%;
+      height: 24px;
+      border: 2px solid gray;      
+      border-radius: 4px;
+    }
+    .btn {
+      width: 100%;
+      height: 36px;
+      background: #96d096;
+      color: white;
+      font-size: large;
+      border: none;
+    }
+    .errMsg {
+      color: red;
+    }
+    .validMsg {
+      display: none;
+    }
+  }
   }
 `;
